a30fab1ff6993c02869d2c42f2d5e9b6f03f4955
discoFries91 <j.mulevicius@gmail.com>
2022-04-26 20:10:39 +0300

    Commit script file

---
+++ PirmaPaskaita/script.js
@@ -0,0 +1,16 @@
+// One line komentaras
+
+/*
+  Sveiki čia yra 
+  Multi lane komentaras
+*/
+
+
+// Sukuriame kintamuosius
+let myName; 
+let age;
+
+// Priskiriam kintamiesiems reikšmes
+// rasoma tarp dvigubu arba viengubu kabuciu
+myName = "Rokas";
+age = 23;
